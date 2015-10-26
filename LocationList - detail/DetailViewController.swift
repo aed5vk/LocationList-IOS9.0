0631fb474a80d65b0bc7b38099a98c35452cecee
aerikdan <adrian321@gmail.com>
2015-10-26 18:12:38 -0400

    adds detail folder

---
+++ LocationList - detail/DetailViewController.swift
@@ -0,0 +1,56 @@
+//
+//  DetailViewController.swift
+//  LocationList - detail
+//
+//  Created by Adrian Dan on 10/26/15.
+//  Copyright © 2015 Adrian Dan. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+    
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let initialLocation = CLLocation(latitude: 38.035657, longitude: -78.503321)
+        centerMapOnLocation(initialLocation)
+        
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var mapWidget: MKMapView!
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation){
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius*2.0)
+        mapWidget.setRegion(coordinateRegion, animated: true)
+    }
+
+
+}
+
