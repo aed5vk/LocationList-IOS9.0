3c9a399eee308f85c6d9f98c9444f3166d8db2a7
aerikdan <adrian321@gmail.com>
2015-11-22 14:21:30 -0500

    adds in the get() and post() functions from the online tutorial

--- LocationList - detail/MasterViewController.swift
+++ LocationList - detail/MasterViewController.swift
@@ -328,6 +328,108 @@ class MasterViewController: UITableViewController, CLLocationManagerDelegate {
         }
     }
 
+    //MARK: Web Services Stuff
+    func get(){
+    
+        let postEndpoint: String = "http://jsonplaceholder.typicode.com/posts/1"
+        guard let url = NSURL(string: postEndpoint) else {
+            print("Error: cannot create URL")
+            return
+            }
+        let urlRequest = NSURLRequest(URL: url)
+    
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        
+        let task = session.dataTaskWithRequest(urlRequest, completionHandler: { (data, response, error) in
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
+            guard error == nil else {
+                print("error calling GET on /posts/1")
+                print(error)
+                return
+            }
+            // parse the result as JSON, since that's what the API provides
+            let post: NSDictionary
+            do {
+                post = try NSJSONSerialization.JSONObjectWithData(responseData,
+                    options: []) as! NSDictionary
+            } catch  {
+                print("error trying to convert data to JSON")
+                return
+            }
+            // now we have the post, let's just print it to prove we can access it
+            print("The post is: " + post.description)
+            
+            // the post object is a dictionary
+            // so we just access the title using the "title" key
+            // so check for a title and print it if we have one
+            if let postTitle = post["title"] as? String {
+                print("The title is: " + postTitle)
+            }
+        })
+        task.resume()
+    }
+    
+    func post(){
+        
+        let postsEndpoint: String = "http://jsonplaceholder.typicode.com/posts"
+        guard let postsURL = NSURL(string: postsEndpoint) else {
+            print("Error: cannot create URL")
+            return
+        }
+        let postsUrlRequest = NSMutableURLRequest(URL: postsURL)
+        postsUrlRequest.HTTPMethod = "POST"
+        
+        let newPost: NSDictionary = ["title": "Frist Psot", "body": "I iz fisrt", "userId": 1]
+        do {
+            let jsonPost = try NSJSONSerialization.dataWithJSONObject(newPost, options: [])
+            postsUrlRequest.HTTPBody = jsonPost
+            
+            let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+            let session = NSURLSession(configuration: config)
+            
+            let task = session.dataTaskWithRequest(postsUrlRequest, completionHandler: {
+                (data, response, error) in
+                guard let responseData = data else {
+                    print("Error: did not receive data")
+                    return
+                }
+                guard error == nil else {
+                    print("error calling GET on /posts/1")
+                    print(error)
+                    return
+                }
+                
+                // parse the result as JSON, since that's what the API provides
+                let post: NSDictionary
+                do {
+                    post = try NSJSONSerialization.JSONObjectWithData(responseData,
+                        options: []) as! NSDictionary
+                } catch  {
+                    print("error parsing response from POST on /posts")
+                    return
+                }
+                // now we have the post, let's just print it to prove we can access it
+                print("The post is: " + post.description)
+                
+                // the post object is a dictionary
+                // so we just access the title using the "title" key
+                // so check for a title and print it if we have one
+                if let postID = post["id"] as? Int
+                {
+                    print("The ID is: \(postID)")
+                }
+            })
+            
+            task.resume()
+        }catch {
+            print("Error: cannot create JSON from post")
+        }
+        
+    }
 
 }
 
